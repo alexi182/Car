5e4b2c4a9cb1e27bcd089cdc85114e4e80bbed85
kurdin <kurdin@itopcase.ru>
2019-03-26 14:42:12 +0300

    in progress

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './stores/store';
-import Index from './pages/Index/Index';
-import ProductPage from './pages/Products/Products'
+import Layout from './layout/Layout';
 
 let page = document.getElementById('page');
 
 ReactDOM.render(
-    <Provider store={store} >
-       <BrowserRouter>
-          <Switch>
-             <Route exact path="/" component={Index} />
-             <Route exact path="/products" component={ProductPage} />
-          </Switch>
-       </BrowserRouter>
+    <Provider store={store}>
+       <Layout />
     </Provider>
-    , page);
+    , page
+);
 
 
 
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import reduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
 import actionReducers from '../reducers/actions';
 
 const reducer = combineReducers({
-   actions: actionReducers,
-
+   actions: actionReducers
 });
 
-const store = createStore(reducer, applyMiddleware(reduxThunk));
+const store = createStore(reducer, applyMiddleware(reduxThunk, logger));
 
 export default store;
